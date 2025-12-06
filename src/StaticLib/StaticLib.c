b0057cbf9128786310dafe6bf7c4d5ac6052bdc0
g2527056-del <g2527056@st.t-kougei.ac.jp>
2025-12-07 01:37:22 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -34,38 +34,83 @@ void finalize(QUEUE* q)
 // valの値をキューに入れる。実行の成否を返す
 bool enqueue(QUEUE* q, int val)
 {
-	// ToDo: valのデータをキューに追加します
-	// 上手くいかない場合にはfalseを返します
-	// メモリを使い切ったら先頭アドレスに戻って追加して下さい
+	
+	// 上手くいかない場合にはfalse
+	
+	if (q == NULL) return false;
 
-	return false;
+	int max = getMaxCount(q);
+	if (max <= 0) return false;
+
+	// 空きがない場合は false
+	if (countQueueableElements(q) <= 0) return false;
+
+	*(q->head) = val;
+	q->head++;
+
+	// メモリを使い切ったら先頭アドレスに戻って追加して下さい
+	if (q->head >= q->memory_end) 
+	{
+		q->head = q->memory_begin;
+	}
+
+	return true;
+	
 }
 
 
 // addrから始まるnum個の整数をキューに入れる。実行の成否を返す
 bool enqueue_array(QUEUE* q, int* addr, int num)
 {
-	// ToDo: addrからnum個のデータをキューに追加します
-	// 上手くいかない場合にはfalseを返します
+	
+	// 上手くいかない場合にはfalse
+	
+	if (q == NULL ||  addr == NULL || num <= 0) return false;
+	if (countQueueableElements(q) < num) return false;
 	// メモリを使い切ったら先頭アドレスに戻って追加して下さい
-
-	return false;
+	for (int i = 0; i < num; i++) {
+		if (!enqueue(q, addr[i])) return false;
+	}
+	return true;
 }
 
 // キューから一つの要素を取り出す(不具合時は0を返す)
 int dequeue(QUEUE* q)
 {
-	// ToDo: 先頭のデータを返します
+	
 
-	return 0;
+	if (q == NULL) return 0;
+
+	// 空なら取り出せない
+	if (isEmpty(q)) return 0;
+
+	int val = *(q->tail);
+	q->tail++;
+
+	// 末尾まで行ったら先頭へ
+	if (q->tail >= q->memory_end) 
+	{
+		q->tail = q->memory_begin;
+	}
+
+	return val;
 }
 
 // addrにキューからnumの要素を取り出す。取り出せた個数を返す
 int dequeue_array(QUEUE* q, int* addr, int num)
 {
-	// ToDo: 先頭からnum個のデータをaddrに格納します
+	
+	if (q == NULL  || addr == NULL || num <= 0) return 0;
+	//先頭からnum個のデータをaddrに格納
+	int count = 0;
+	for (int i = 0; i < num; i++) 
+	{
+		if (isEmpty(q)) break;
 
-	return 0;
+		addr[i] = dequeue(q);
+		count++;
+	}
+	return count;
 }
 
 // キューが空かどうかを調べる
